77acbdc8ae2f8f85753a29304d61f0d21c890292
nuriaRZ <nuriesma99@gmail.com>
2020-03-09 17:43:17 +0100

    2

--- VentaDeCochesJPA/src/modelo/GestionFabricante.java
+++ VentaDeCochesJPA/src/modelo/GestionFabricante.java
@@ -50,7 +50,7 @@ public class GestionFabricante {
 				modificacion();
 				break;
 			case 4: 
-				
+				baja();
 				break;
 			}
 		} while (opcionElegida != 0);
@@ -61,7 +61,7 @@ public class GestionFabricante {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("VentaDeCoches");
 		EntityManager em = entityManagerFactory.createEntityManager();
 		
-		Query q = em.createNamedQuery("Coche.findAll");
+		Query q = em.createNamedQuery("Fabricante.findAll");
 		
 		List<Fabricante> fabricantes = (List<Fabricante>) q.getResultList();
 		
@@ -97,7 +97,7 @@ public class GestionFabricante {
 		em.close();
 	}
 	
-	private static void modificacion() {
+	private static void modificacion() throws ParseException, ErrorBBDDException {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("VentaDeCoches");
 
 		EntityManager em = entityManagerFactory.createEntityManager();
@@ -105,9 +105,9 @@ public class GestionFabricante {
 		do {
 			System.out.println("\n\tIntroduzca ID del fabricante ('-1' - ver listado, '0' - salir): ");
 			id = Utils.getIntConsola(-1);
-			if (id == -1) {
-				listado();
-			}else {
+			if (id == -1) listado();
+			if (id == 0) Main.menuPrincipal();
+			else {
 				TypedQuery<Fabricante> q = em.createQuery("SELECT f FROM Fabricante as f where f.id = "+ id, Fabricante.class);
 				
 				List<Fabricante> fabricantes = q.getResultList();
@@ -116,7 +116,10 @@ public class GestionFabricante {
 				for (Fabricante fabEnLista : fabricantes) {
 					fabEnLista.setNombre("Modificado");
 					em.persist(fabEnLista);
-				}	
+				}
+				em.getTransaction().commit();
+				
+				em.close();
 			}
 		}while(id != 0);
 		
@@ -124,9 +127,36 @@ public class GestionFabricante {
 		
 
 		
-		em.getTransaction().commit();
 		
-		em.close();
+	}
+	
+	private static void baja() throws ParseException, ErrorBBDDException {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("VentaDeCoches");
+
+		EntityManager em = entityManagerFactory.createEntityManager();
+		int id = -2;
+		do {
+			System.out.println("\n\tIntroduzca ID del fabricante ('-1' - ver listado, '0' - salir): ");
+			id = Utils.getIntConsola(-1);
+			if (id == -1) listado();
+			if (id == 0) Main.menuPrincipal();
+			else {
+				TypedQuery<Fabricante> q = em.createQuery("SELECT f FROM Fabricante as f where f.cif = '"+ id+"'", Fabricante.class);
+				
+				List<Fabricante> fabricantes = q.getResultList();
+				
+				em.getTransaction().begin();
+				for (Fabricante fabEnLista : fabricantes) {
+					em.remove(fabEnLista);
+				}
+				em.getTransaction().commit();
+				
+				
+				em.close();
+			}
+		}while(id != 0);
+		
+		
 	}
 	
 	
